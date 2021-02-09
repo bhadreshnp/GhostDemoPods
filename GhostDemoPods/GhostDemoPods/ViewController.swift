77c507ae2bd9ce19194726a8f4890ff048caaa11
Bhadresh Radadiya <bhadresh.radadiya@gmail.com>
2021-02-09 11:02:57 -0500

    created project

---
+++ GhostDemoPods/GhostDemoPods/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GhostDemoPods
+//
+//  Created by Bhadresh Radadiya on 2021-02-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
